7a453b07b28b54680ba1ccfbd65d67746e33c77d
freshmea <freshmea@naver.com>
2022-01-24 00:10:35 +0900

    Initial commit

---
+++ code.py
@@ -0,0 +1,12 @@
+import board
+import neopixel
+
+# Update this to match the number of NeoPixel LEDs connected to your board.
+num_pixels = 30
+
+pixels = neopixel.NeoPixel(board.GP0, num_pixels)
+pixels.brightness = 0.5
+
+while True:
+    pixels.fill((255, 0, 0))
+
